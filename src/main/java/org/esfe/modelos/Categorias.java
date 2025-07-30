40d2fbc47fc036f4159e4414aaea05cd36175ead
ST24002@esfe.agape.edu.sv <Letinic@outlook.com>
2025-07-30 17:26:22 -0600

    entidad de categorias ya realizadas

---
+++ src/main/java/org/esfe/modelos/Categorias.java
@@ -0,0 +1,28 @@
+package org.esfe.modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "categorias")
+public class Categorias {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre  es requerida")
+    private String nombre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
