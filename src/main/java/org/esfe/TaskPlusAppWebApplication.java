991fe1f2a8c3fd816e6f21dbc299937c763ad2bb
Jairo-gd <gd24002@esfe.agape.edu.sv>
2025-07-29 20:22:08 -0600

    estructura del proyecto y configuración de layout

---
+++ src/main/java/org/esfe/TaskPlusAppWebApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlusAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlusAppWebApplication.class, args);
+	}
+
+}
